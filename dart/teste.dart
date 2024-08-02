c6017f2c477f7d77ae59a96a6037f3072b7b87e5
Rodrigo Bossini <professorbossini@gmail.com>
2024-08-02 12:54:01 -0300

    g3(ap01-dart): encerra secao 2.12

---
+++ dart/teste.dart
@@ -0,0 +1,62 @@
+void main(){
+  int a = 2;
+  ++a;
+  print(a);
+  a++;
+  print(a);
+  print(++a);
+  print(a++);
+  print(a);
+  // double dividendo = 6;
+  // int divisor = 2;
+  // num quociente = 3 / 2;
+  // print(quociente.runtimeType);
+  // double a = 1.2;
+  // int b = a.round();
+  // b = a.floor();
+  // b = a.ceil();
+  // double d = 1;
+  // print(d.runtimeType);
+  // int a = 1;
+  // double b = a;
+  //conversões
+  // de string para int
+  // String idadeTextual = '25';
+  // int idade = int.parse(idadeTextual);
+  // print(idade);
+
+  // String pesoTextual = "80.2";
+  // double peso = double.parse(pesoTextual);
+  // print(peso);
+  // print(peso.runtimeType);
+
+  // String alturaTextual = "1.2";
+  // num altura = int.parse(alturaTextual);
+  // print(altura);
+  // print(altura.runtimeType);
+
+  //multiplicação de strings
+  // String letra = 'x';
+  // print(letra * 10);
+  // //concatenação
+  // String nome = "Pedro";
+  // int idade = 22;
+  // print("Me chamo " + nome);
+  // print("Tenho " + idade.toString() + " anos");
+  // //interpolação
+  // print('Me chamo $nome');
+  // print("No ano que vem vou ter ${idade + 1} anos");
+  // String nome = "João";
+  // print(nome);
+  // print(nome.runtimeType);
+  // String teste = 'Use \n para pular uma linha';
+  // print(teste);
+
+  /*
+    esse é um
+    comentário
+    de múltiplas
+    linhas
+   */
+  //print('Hello, World');
+}
