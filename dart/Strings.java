f3feff74c88ba7a352cb1b8bbad7693eecefec83
Rodrigo Bossini <professorbossini@gmail.com>
2024-08-05 09:11:27 -0300

    g1(ap01-dart): estamos na secao 2.13
    - retomar com inferencia

---
+++ dart/Strings.java
@@ -0,0 +1,8 @@
+String s1 = "abc";
+String s2 = "abc";
+String s3 = JOP.sID("Digite uma string");
+String s4 = "abc";
+sysout(s1 == s2);
+sysout(s3 == s4);
+
+s1.toUpperCase();
--- dart/teste.dart
+++ dart/teste.dart
@@ -1,62 +1,87 @@
 void main(){
-  int a = 2;
-  ++a;
-  print(a);
-  a++;
-  print(a);
-  print(++a);
-  print(a++);
-  print(a);
-  // double dividendo = 6;
-  // int divisor = 2;
-  // num quociente = 3 / 2;
-  // print(quociente.runtimeType);
-  // double a = 1.2;
-  // int b = a.round();
-  // b = a.floor();
-  // b = a.ceil();
-  // double d = 1;
-  // print(d.runtimeType);
-  // int a = 1;
-  // double b = a;
-  //conversões
-  // de string para int
-  // String idadeTextual = '25';
-  // int idade = int.parse(idadeTextual);
-  // print(idade);
+  // int a = 2;
+  // print(a.runtimeType);
 
-  // String pesoTextual = "80.2";
-  // double peso = double.parse(pesoTextual);
-  // print(peso);
-  // print(peso.runtimeType);
-
-  // String alturaTextual = "1.2";
-  // num altura = int.parse(alturaTextual);
-  // print(altura);
-  // print(altura.runtimeType);
-
-  //multiplicação de strings
-  // String letra = 'x';
-  // print(letra * 10);
-  // //concatenação
-  // String nome = "Pedro";
-  // int idade = 22;
-  // print("Me chamo " + nome);
-  // print("Tenho " + idade.toString() + " anos");
-  // //interpolação
-  // print('Me chamo $nome');
-  // print("No ano que vem vou ter ${idade + 1} anos");
-  // String nome = "João";
-  // print(nome);
-  // print(nome.runtimeType);
-  // String teste = 'Use \n para pular uma linha';
-  // print(teste);
-
-  /*
-    esse é um
-    comentário
-    de múltiplas
-    linhas
-   */
-  //print('Hello, World');
+  // dynamic b = 2;
+  // print(b.runtimeType);
+  // b = 2.5;
+  // print(b.runtimeType);
 }
+
+
+// void main(){
+//   int a = 2;
+//   ++a;
+//   print(a);//3
+//   a++;
+//   print(a);//4
+//   print(++a);//5
+//   print(a++);//5
+//   print(a);//6
+//   // double d = 1.2;
+//   // // int i = (int) d;
+//   // int i1 = d.round();
+//   // int i2 = d.floor();
+//   // int i3 = d.ceil();
+
+//   //de string para int
+//   // String idadeTextual = "25";
+//   // int idade = int.parse(idadeTextual);
+//   // print(idade);
+
+//   // //de string para double
+//   // String pesoTextual = "80.5";
+//   // double peso = double.parse(pesoTextual);
+//   // print(peso);
+
+//   // //de string para num
+//   // String alturaTextual = "1";
+//   // num altura = num.parse(alturaTextual);
+//   // print(altura);
+//   // print(altura.runtimeType);  
+
+
+//   // String letra = "a";
+//   // print(letra * 10);
+//   //concatenação
+//   // String nome = "João";
+//   // int idade = 20;
+//   // print("Me chamo " + nome);
+//   // print("Tenho " + idade.toString() + " anos");
+
+//   // //interpolação
+//   // print("Meu nome é $nome");
+
+//   // //Terei 21 anos no ano que vem
+//   // print("Terei ${idade + 1} anos no ano que vem");
+
+//   // int i = 2;
+//   // double d = i;
+
+//   // int a = 2;
+//   // print(a);
+//   // print(a.runtimeType);
+
+//   // double d = 2.5;
+//   // print(d);
+//   // print(d.runtimeType);
+
+//   // double d2 = 2;
+//   // print(d2);
+//   // print(d2.runtimeType);
+
+//   // num n1 = 1;
+//   // num n2 = 1.1;
+
+//   // print(n1.runtimeType);
+//   // print(n2.runtimeType);
+
+//   // String teste = r"isso pula uma linha: \n. Viu?";
+//   // print(teste);
+  
+//   /*
+//     comentário
+//     de múltiplas linhas
+//    */
+//   //print("Hello, World");
+// }
