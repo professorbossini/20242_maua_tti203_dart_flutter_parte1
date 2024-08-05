3c79bbf308c01d6e2e65e8c84d67dd872f3804fc
Rodrigo Bossini <professorbossini@gmail.com>
2024-08-05 10:38:28 -0300

    g1-t2(ap01-dart): fim da apostila

--- dart/teste.dart
+++ dart/teste.dart
@@ -1,87 +1,181 @@
 void main(){
-  // int a = 2;
-  // print(a.runtimeType);
-
-  // dynamic b = 2;
-  // print(b.runtimeType);
-  // b = 2.5;
-  // print(b.runtimeType);
+  var mediaFinal = 5;
+  final conceito = switch(mediaFinal){
+    10 || 9 => 'A',
+    8 => 'B',
+    _ => 'R'
+  };
+  // var idade = 18;
+  // // var podeDirigir = idade >= 18 ? "Sim" : "Nâo";
+  // var podeDirigir = if(idade >= 18) "Sim" else "Não";
+  // double nota = 9.7;
+  // // switch statement
+  // switch(nota){
+  //   case > 9 && <= 10:
+  //     print ("Nota 10");
+  // }
+  // var frutas = ["banana", "laranja"];
+  // switch(frutas){
+  //   case ['Banana', 'laranja']:
+  //     print("banana e laranja");
+  //   case ['banana', 'maçã']:
+  //     print("banana e maçã");
+  // }
+  // var vaiChover = "Sim";
+  // switch(vaiChover){
+  //   case "Sim":
+  //     print ("Leve guarda chuva");
+  //   default:
+  //     print ("Não leve");
+  // }
+  // const nota = 10;
+  // switch(nota){
+  //   case 10 || 9:
+  //     print("A");
+  //   default:
+  //     print ("R");
+  // }
 }
+// // class Pessoa{
+// //   int idade;
+// // }
+// // class Null{}
 
-
+// // Null null = new Null();
 // void main(){
-//   int a = 2;
-//   ++a;
-//   print(a);//3
-//   a++;
-//   print(a);//4
-//   print(++a);//5
-//   print(a++);//5
-//   print(a);//6
-//   // double d = 1.2;
-//   // // int i = (int) d;
-//   // int i1 = d.round();
-//   // int i2 = d.floor();
-//   // int i3 = d.ceil();
+//   const n1 = 2;
+//   const n2 = 2;
+//   const soma = n1 + n2;
+//   print(soma);
+//   print(soma.runtimeType);
+//   var n4 = 3;
+//   // const n5 = n4 + 2;
+//   final n6 = n4 + 2;
+//   print(n6);
+//   print(n6.runtimeType);
+//   //inferência de tipos para constantes
+//   //const e final
+//   // const nome = "Ana";
+//   // print(nome);
+//   // print(nome.runtimeType);
 
-//   //de string para int
-//   // String idadeTextual = "25";
-//   // int idade = int.parse(idadeTextual);
-//   // print(idade);
+//   // const String sobrenome = "Silva";
 
-//   // //de string para double
-//   // String pesoTextual = "80.5";
-//   // double peso = double.parse(pesoTextual);
-//   // print(peso);
-
-//   // //de string para num
-//   // String alturaTextual = "1";
-//   // num altura = num.parse(alturaTextual);
-//   // print(altura);
-//   // print(altura.runtimeType);  
-
-
-//   // String letra = "a";
-//   // print(letra * 10);
-//   //concatenação
-//   // String nome = "João";
-//   // int idade = 20;
-//   // print("Me chamo " + nome);
-//   // print("Tenho " + idade.toString() + " anos");
-
-//   // //interpolação
-//   // print("Meu nome é $nome");
-
-//   // //Terei 21 anos no ano que vem
-//   // print("Terei ${idade + 1} anos no ano que vem");
-
-//   // int i = 2;
-//   // double d = i;
-
-//   // int a = 2;
-//   // print(a);
-//   // print(a.runtimeType);
-
-//   // double d = 2.5;
-//   // print(d);
-//   // print(d.runtimeType);
-
-//   // double d2 = 2;
-//   // print(d2);
-//   // print(d2.runtimeType);
-
-//   // num n1 = 1;
-//   // num n2 = 1.1;
-
-//   // print(n1.runtimeType);
-//   // print(n2.runtimeType);
-
-//   // String teste = r"isso pula uma linha: \n. Viu?";
+//   // const nomeCompleto = "$nome" + " " + "$sobrenome";
+//   // print(nomeCompleto);
+//   // print(nomeCompleto.runtimeType);
+//   // nome = 1;
+//   // final nome = "João";
+//   // print(nome.runtimeType);
+//   // // final peso;
+//   // // print(peso);
+//   // // print(peso.runtimeType);
+//   // var teste = null;
 //   // print(teste);
-  
-//   /*
-//     comentário
-//     de múltiplas linhas
-//    */
-//   //print("Hello, World");
+//   // print(teste.runtimeType);
+//   // teste = 2;
+//   // print(teste.runtimeType);
+//   // teste = "abc";
+//   // print(teste.runtimeType);
+//   // nome = "Pedro";
+//   // print(nome.runtimeType);    
+//   //inferência de tipos
+//   // var nome = "João";
+//   // print(nome.runtimeType);
+//   // //não pode 
+//   // // nome = 1;
+//   // var idade;
+//   // print(idade.runtimeType);
+//   // idade = 17;
+//   // print(idade.runtimeType);
+//   // idade = "abc";
+//   // print(idade.runtimeType);
 // }
+
+// // void main(){
+// //   // int a = 2;
+// //   // print(a.runtimeType);
+
+// //   // dynamic b = 2;
+// //   // print(b.runtimeType);
+// //   // b = 2.5;
+// //   // print(b.runtimeType);
+// // }
+
+
+// // void main(){
+// //   int a = 2;
+// //   ++a;
+// //   print(a);//3
+// //   a++;
+// //   print(a);//4
+// //   print(++a);//5
+// //   print(a++);//5
+// //   print(a);//6
+// //   // double d = 1.2;
+// //   // // int i = (int) d;
+// //   // int i1 = d.round();
+// //   // int i2 = d.floor();
+// //   // int i3 = d.ceil();
+
+// //   //de string para int
+// //   // String idadeTextual = "25";
+// //   // int idade = int.parse(idadeTextual);
+// //   // print(idade);
+
+// //   // //de string para double
+// //   // String pesoTextual = "80.5";
+// //   // double peso = double.parse(pesoTextual);
+// //   // print(peso);
+
+// //   // //de string para num
+// //   // String alturaTextual = "1";
+// //   // num altura = num.parse(alturaTextual);
+// //   // print(altura);
+// //   // print(altura.runtimeType);  
+
+
+// //   // String letra = "a";
+// //   // print(letra * 10);
+// //   //concatenação
+// //   // String nome = "João";
+// //   // int idade = 20;
+// //   // print("Me chamo " + nome);
+// //   // print("Tenho " + idade.toString() + " anos");
+
+// //   // //interpolação
+// //   // print("Meu nome é $nome");
+
+// //   // //Terei 21 anos no ano que vem
+// //   // print("Terei ${idade + 1} anos no ano que vem");
+
+// //   // int i = 2;
+// //   // double d = i;
+
+// //   // int a = 2;
+// //   // print(a);
+// //   // print(a.runtimeType);
+
+// //   // double d = 2.5;
+// //   // print(d);
+// //   // print(d.runtimeType);
+
+// //   // double d2 = 2;
+// //   // print(d2);
+// //   // print(d2.runtimeType);
+
+// //   // num n1 = 1;
+// //   // num n2 = 1.1;
+
+// //   // print(n1.runtimeType);
+// //   // print(n2.runtimeType);
+
+// //   // String teste = r"isso pula uma linha: \n. Viu?";
+// //   // print(teste);
+  
+// //   /*
+// //     comentário
+// //     de múltiplas linhas
+// //    */
+// //   //print("Hello, World");
+// // }
