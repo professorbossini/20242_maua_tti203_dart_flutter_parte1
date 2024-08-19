bdec2bc0166366309b3728be0528695fd627c4ea
Rodrigo Bossini <professorbossini@gmail.com>
2024-08-19 09:57:57 -0300

    g1-t2(ap02-dart): propoe exercicio ped/pap/tes
    - retomar a partir da pg 16, resolvendo com funcoes

---
+++ pedra_papel_tesoura/lib/pedra_papel_tesoura.dart
@@ -0,0 +1,27 @@
+//importar o pacote para entrada e saída de dados
+
+//importar o pacote para gerar valores aleatórios
+
+//fazer um enum com as opções pedra, papel, tesoura e sair
+void jogo(){
+  int opUsuario;
+  do{
+
+    //exibir o menu (1 para pedra...)
+
+    //se o usuário digitar 4, sair, senão
+
+    //sortear escolha do computador
+
+    //mapear a opção do usuário(int) para enum
+
+    //mapear a opção do computador(int) para enum
+
+    //exibir as opções de cada um JOGADOR(tesoura) vs (papel)COMPUTADOR
+
+    //decidir quem venceu ou se houve empate
+
+    //mostrar o resultado
+
+  }while (opUsuario != 4);
+}
