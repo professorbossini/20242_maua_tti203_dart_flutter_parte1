3fe7998315a658b5cc387f21d67342d08e0d8770
Rodrigo Bossini <professorbossini@gmail.com>
2024-08-22 11:41:36 -0300

    g2: propoe pedra_papel_tesoura
    - esse foi apenas equiparando turmas

---
+++ dart/ap02.dart
@@ -0,0 +1,59 @@
+
+
+// try{
+//   String s = null;
+//   s.toUpperCase();
+// }
+// catch (NullPointerException e){
+
+// }
+
+// import 'dart:io';
+// void main(){
+//   stdout.writeln("Digite a idade");
+//   String? s = stdin.readLineSync();
+//   if(s == null){
+//     int idade = int.parse(s!);
+//   }
+//   // stdout.writeln('Digite seu nome');
+//   // //sound null safety
+//   // String? nome = stdin.readLineSync();
+//   // String? m = nome?.toUpperCase();
+// }
+
+
+
+
+
+// // void main(){
+// //   stdout.writeln('Digite seu nome');
+// //   // System.out.println
+// //   // stdin
+// //   //System.in
+// //   var nome = stdin.readLineSync();
+// //   stdout.writeln('Oi, $nome');
+// // }
+
+
+// // enum DiaDaSemana{segundaFeira, tercaFeira}
+// // void main(){
+// //   print(DiaDaSemana.values);
+// //   DiaDaSemana dia = DiaDaSemana.segundaFeira;
+// //   switch(dia){
+// //     case DiaDaSemana.segundaFeira:
+// //       print('NAOOOO');
+// //     default:
+// //       // print("R");
+// //   }
+//   // const dia = 1;
+//   // switch(dia){
+//   //   case 1:
+//   //     print('NAAAO');
+//   // }
+//   // const nomes = [1.1, 1];
+
+
+//   // for(final nome in nomes){
+//   //   print(nome);
+//   // }
+// // }
---
+++ dart/pedra_papel_tesoura/lib/pedra_papel_tesoura.dart
@@ -0,0 +1,22 @@
+//importar a biblioteca para entrada/saída
+
+//importar a biblioteca para geração de valores aleatórios
+
+//definir um enum com as opções (pedra...)
+void jogo(){
+  //fazer o loop (do/while) do jogo, se o usuário digitar 4, ele sai
+
+  //exibir o menu
+  //capturar a opção do usuário, validando com do/while
+
+  //sortear a opção do computador
+
+  //mapear a opção do usuário, de int para enum
+
+  //mapear a opção do computador, de int para enum
+
+  //exibir as escolhas
+  //JOGADOR(pedra) vs (tesoura)COMPUTADOR
+
+  //verificar o exibir o resultado: jogador ganhou, computador ganhou ou empate
+}
