346404a8af0fbff3b4b5d045178e1d8bd3a3cab6
Rodrigo Bossini <professorbossini@gmail.com>
2024-08-22 11:56:53 -0300

    g1-si(ap02-dart): encerra a apostila
    - jogo pedra_papel_tesoura proposto
    - retomar da apostila 03

---
+++ dart/pedra_papel-tesoura/bin/pedra_papel_tesoura.dart
@@ -0,0 +1,5 @@
+import 'package:pedra_papel_tesoura/pedra_papel_tesoura.dart' as pedra_papel_tesoura;
+
+void main(List<String> arguments) {
+  pedra_papel_tesoura.jogo();
+}
---
+++ dart/pedra_papel-tesoura/lib/pedra_papel_tesoura.dart
@@ -0,0 +1,28 @@
+//importar a biblioteca de entrada/saida
+
+//importar a biblioteca para gerar valores aleatórios
+
+//definir um enum com as opções do jogo
+//são 4: pedra....sair
+
+void jogo(){
+  //fazer um do/while para representar o loop do jogo
+  //enquanto o usuário não digitar 4, o jogo continua
+
+    //exibir o menu
+    //capturar a opção do usuário, validando com do/while
+
+    //sortear a opção do computador
+
+    //dica: lembre de usar a lista chamada values do enum
+    //mapear a opção do usuário, de int para enum
+
+    //mapear a opção do computador, de int para enum
+
+    //exibir as opções
+    //JOGADOR(pedra) vs (tesoura)COMPUTADOR
+
+    //verificar e exibir o resultado
+    //jogador venceu, computador venceu ou empate
+
+}
