c9b3d0747a9d5913b30398c1328f7bd7c326be1f
Rodrigo Bossini <professorbossini@gmail.com>
2024-08-23 12:49:25 -0300

    g3(ap03-dart): encerra assunto lista
    - retomar falando sobre tuplas

---
+++ dart/colecoes/bin/colecoes.dart
@@ -0,0 +1,21 @@
+import 'package:colecoes/colecoes.dart' as colecoes;
+void main(List <String> arguments){
+  // arguments.forEach()
+}
+
+
+
+
+// void main(List<String> arguments) {
+//   var itensDiversos = ["Ana", true, 2, 2.5];
+//   print(itensDiversos.runtimeType);
+//   // var nomes = ["Ana", "Pedro"];
+//   // nomes[0] = "Ana Paula";
+//   // print(nomes);
+//   // print(nomes);
+//   // print(nomes.toString());
+//   // print(nomes.runtimeType);
+//   // print(nomes[0]);
+//   // print(nomes[1]);
+//   // print(nomes[2]);
+// }
