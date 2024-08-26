dbb5edc0fe4ad8a4166b89d5ab83ac55a9d97c3a
Rodrigo Bossini <professorbossini@gmail.com>
2024-08-26 09:05:43 -0300

    g1(ap03-dart): encerra assunto listas
    - retomar propondo jogo da mega sena
    - comecar a falar sobre tuplas

---
+++ dart/colecoes/bin/colecoes.dart
@@ -0,0 +1,68 @@
+import 'package:colecoes/colecoes.dart' as colecoes;
+
+void main(List<String> arguments) {
+
+  List <List <String?>?>? eAgora = [[null]];
+  //null
+  //[null]
+  //[[null]]
+
+  // List<String?>? eAgora;
+  //null
+  //[null]
+
+  // List<String>? eAgora;
+  // //null
+  // //[null]
+  // //[1] 
+
+  //type annotation
+  // var nomes = <String?> [null];
+  // nomes.add(null);
+
+  // var nomes = [];
+  // nomes.add("Ana");
+  // nomes.add(null);
+  // List <String?> nomes = ["Ana", "João"];
+  // nomes.add(null);
+
+  // //nomes = ["Pedro"];
+  // nomes.add("A");
+  // // nomes[0] = "Jaqueline";
+  // print(nomes);
+  // var l1 = [2];
+  // List l2 = [2];
+  // var letras = [];
+  // print(letras.first);
+  // print(letras.firstOrNull);
+  // print(letras.isEmpty);
+  // print(letras.isNotEmpty);
+  // print(letras.reversed);
+  // print(letras.first);
+  // print(letras.firstOrNull);
+  // print(letras.last);
+  // print(letras.lastOrNull);
+  // //1m32s
+  // //escrever um programa que usa um for regular para somar os elementos contidos em arguments
+  // num soma = 0;
+  // for (final arg in arguments){
+  //   soma += num.parse(arg);
+  // }
+  // print(soma);
+
+  //dart run colecoes 1 2 3 
+
+  //desafio
+  //+55s
+  //estudar como funciona a função forEach que as listas possuem e refazer
+  //arguments.forEach();
+  //arguments.reduce();
+
+  // List<num> numeros = [1, 1.1];
+  // print(numeros[2]);
+
+  // // List<Object> nomes = ["Ana", "Pedro", 1];
+  // // nomes[1] = 2.4;
+  // // nomes[0].falar();
+
+}
