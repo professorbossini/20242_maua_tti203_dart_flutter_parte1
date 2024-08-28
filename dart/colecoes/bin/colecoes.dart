8de6c8591cebd73c00ab22158e865def7c32180e
Rodrigo Bossini <professorbossini@gmail.com>
2024-08-28 12:56:11 -0300

    g2-t3(ap-03-dart): encerra listas
    - retomar falando sobre tuplas

---
+++ dart/colecoes/bin/colecoes.dart
@@ -0,0 +1,58 @@
+import 'package:colecoes/colecoes.dart' as colecoes;
+import 'dart:io';
+import 'dart:math';
+void main(List<String> arguments) {
+
+  List < List <String?> >? lista = [[null]];
+
+  // List <String?>? letras = [null];
+
+  // String? nome;
+  // var letras = [];
+  // letras.add(nome);
+  // var letras2 = ["A", "B"];
+  // letras2.add(nome);
+  // final jogoUsuario = <int> [];
+  // final jogoComputador = <int> [];
+  // jogoUsuario ==
+  // int numero;
+  // do{
+  //   print("Digite um numero");
+  //   numero = int.parse(stdin.readLineSync()!);
+  //   if(!jogoUsuario.contains(numero)) jogoUsuario.add(numero);
+  // }while(numero < 1 || numero > 60 || jogoUsuario.length < 6);
+
+  // var gerador = Random();
+  // while (jogoComputador.length < 6){
+  //   final valor = gerador.nextInt(6) + 1;
+  //   if(!jogoComputador.contains(valor)) jogoComputador.add(valor);
+  // }
+
+  // jogoUsuario.sort();//in-place
+  // jogoComputador.sort();
+
+  // print('JOGADOR($jogoUsuario) vs ($jogoComputador)COMPUTADOR');
+
+  // print("Veja os números que você acertou");
+  // for (final n in jogoUsuario){
+  //   if(jogoComputador.contains(n)) stdout.write("$n ");
+  // }
+
+
+  //type annotation
+  // var strings = <String> [];
+  // List<String> outra = [];
+  // var lista = [];
+  // List<Object> nomes = [1, 1.2];
+  // //for each
+  // for(var nome in nomes){
+  //   print(nome);
+  // }
+  // nomes = ["jflçekwa"];
+  // nomes[0] = "Ana Paula";
+  // print(nomes);
+  // print(nomes[0]);
+  // print(nomes[2]);
+  // print(nomes.runtimeType);
+  //print(arguments);
+}
