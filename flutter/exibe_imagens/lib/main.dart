9b8d13009761b6e4fe7c9fd1fa88d481eaa62398
Rodrigo Bossini <professorbossini@gmail.com>
2024-08-30 12:56:55 -0300

    g3(ap01-flutter): encerra pg 23
    - retomar fazendo exercicio da pg 24

---
+++ flutter/exibe_imagens/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+//material design
+void main(){
+  var app = MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Minhas imagens"),
+      ),
+    )
+  );
+  runApp(app);
+}
