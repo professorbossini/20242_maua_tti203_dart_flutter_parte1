5712703765e56b400b62a9023a2ef91a724f7a72
evandro <ferraz.evandro@outlook.com>
2024-09-13 12:34:12 -0300

    pag 64, decodificando JSON para ImageModel

--- flutter/exibe_imagens/funcoes.dart
+++ flutter/exibe_imagens/funcoes.dart
@@ -1,29 +1,29 @@
 
 void main(){
-  final f1 = (){
+  f1(){
     print('f1');
-  };
+  }
 
-  final f2 = (){
+  f2(){
     print('f2');
     return ('f2');
-  };
+  }
 
-  final f3 = () => print('f3');
+  f3() => print('f3');
 
-  final f4 = () => {
+  f4() => {
     'a': 'Ana',
     'b': 'Betania'
   };
 
-  final f5 = (){
+  f5(){
     print('oi');
     return {
 
     };
-  };
+  }
 
-  final f6 = () => {1, 2};
+  f6() => {1, 2};
 
   f1();
   print(f2());
--- flutter/exibe_imagens/lib/src/app.dart
+++ flutter/exibe_imagens/lib/src/app.dart
@@ -1,29 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class App extends StatefulWidget{
-  State<App> createState(){
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() {
     return AppState();
   }
 }
 
-class AppState extends State <App>{
+class AppState extends State<App> {
   int numeroImagens = 0;
-  @override
 
+  void obterImagem() {
+    var url = Uri.https(
+      "api.pexels.com",
+      "/v1/search",
+      {'query': 'people', 'page': '1', 'per_page': '1'},
+    );
+
+    var req = http.Request('get', url);
+
+    req.headers.addAll({
+      'Authorization':
+          "76ON0ldplc9Crvo332JYBwo2JZF6uHhGaSKbOSdTTnL1ZXOyv71A2kPH"
+    });
+    req.send().then((result) {
+      if (result.statusCode == 200) {
+        // requisição funcionou
+        // filtrar as informações completas do fluxos stream
+        // pegando somente as informações referentes ao
+        // conteúdo de resposta da requisição
+        http.Response.fromStream(result).then((response) {
+          print(response.body);
+        });
+      } else {
+        print("Falhou");
+      }
+    });
+    //http.get(Uri.parse(''));
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Minhas imagens"),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: (){
-            // numeroImagens++;
-            setState((){numeroImagens++;});
-          },
-          child: Icon(Icons.add),
-        ),
-      )
-    );
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Minhas imagens"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: obterImagem,
+        child: const Icon(Icons.add),
+      ),
+      body: Text('$numeroImagens'),
+    ));
   }
 }
