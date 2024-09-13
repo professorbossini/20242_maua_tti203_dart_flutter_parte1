504d10b5899b76f10f502a1e30b98073e79a817c
Evandro Catelani Ferraz <evandro.ferraz@maua.br>
2024-09-13 11:10:39 -0300

    pag 64, decodificar o JSON recebido e converter para  um ImageModel

--- flutter/exibe_imagens/lib/src/app.dart
+++ flutter/exibe_imagens/lib/src/app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 class App extends StatefulWidget{
   @override
   State <App> createState(){
@@ -7,6 +9,39 @@ class App extends StatefulWidget{
 }
 class AppState extends State <App>{
   int numeroImagens = 0;
+
+  void obterImagem(){
+    var url = Uri.https(
+      "api.pexels.com",
+      "/v1/search",
+      {'query':'people','page':'1','per_page':'1'},
+    );
+
+    var req = http.Request('get',url);
+    
+    req.headers.addAll(
+      {
+        'Authorization' : "76ON0ldplc9Crvo332JYBwo2JZF6uHhGaSKbOSdTTnL1ZXOyv71A2kPH"
+      }
+    );
+    req.send().then((result) {
+      if(result.statusCode == 200){
+        // requisição funcionou
+        // filtrar as informações completas do fluxos stream
+        // pegando somente as informações referentes ao
+        // conteúdo de resposta da requisição
+        http.Response.fromStream(result).then((response)
+          {
+            print(response.body);
+          }
+        );
+      }else{
+        print("Falhou");
+      }
+    });
+    //http.get(Uri.parse(''));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,10 +51,7 @@ class AppState extends State <App>{
         ),
         floatingActionButton: FloatingActionButton(
           child: const Icon(Icons.add),
-          onPressed: () {
-            //numeroImagens++;
-            setState(() => numeroImagens++);
-          },
+          onPressed: obterImagem,
         ),
         body: Text('$numeroImagens'),
       )
---
+++ flutter/exibe_imagens/lib/src/models/image_model.dart
@@ -0,0 +1,11 @@
+class ImageModel{
+    late String url;
+    late String alt;
+
+    ImageModel(this.url,this.alt);
+
+    ImageModel.fromJSON(Map<String,dynamic> decodedJSON){
+        url = decodedJSON['photos'][0]['src']['medium'];
+        alt = decodedJSON['photos'][0]['alt'];
+    }
+}
