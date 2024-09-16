4b27a0714670519eff6c09a792b0fd7c1b61a37f
Rodrigo Bossini <professorbossini@gmail.com>
2024-09-16 09:09:18 -0300

    feito no g1 retomar na pg 64 fazendo conversao para json

---
+++ flutter/exibe_imagens/lib/src/models/image_model.dart
@@ -0,0 +1,20 @@
+class ImageModel{
+  late String url;
+  late String alt;
+
+  ImageModel(this.url, this.alt);
+
+  //construtor nomeado
+  // ImageModel.fromJSON(Map<String, dynamic> mapa){
+  //   url = mapa['photos'][0]['src']['medium'];
+  //   alt = mapa['photos'][0]['alt'];
+  // }
+  ImageModel.fromJSON(Map <String, dynamic> mapa) :
+    url = mapa['photos'][0]['src']['medium'],
+    alt = mapa['photos'][0]['alt'];
+
+  // ImageModel(String url, String alt){
+  //   this.url = url;
+  //   this.alt = alt;
+  // }  
+}
