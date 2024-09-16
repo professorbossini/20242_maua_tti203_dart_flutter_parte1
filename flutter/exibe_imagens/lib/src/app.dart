726778c11924fe0b60d078af6da12a94bdbc398b
Rodrigo Bossini <professorbossini@gmail.com>
2024-09-16 11:27:44 -0300

    retomar na pg 64 fazendo, depois de async/await
    - fazendo conversao json/mapa/imagem

--- flutter/exibe_imagens/lib/src/app.dart
+++ flutter/exibe_imagens/lib/src/app.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'models/image_model.dart';
 
-class App extends StatefulWidget{
-  State <App> createState(){
+class App extends StatefulWidget {
+  State<App> createState() {
     return AppState();
   }
 }
 
-
-class AppState extends State <App> {
+class AppState extends State<App> {
   int numeroImagens = 0;
+
+  void obterImagem() async {
+    var url = Uri.https(
+      'api.pexels.com',
+      '/v1/search',
+      {'query': 'people', 'page': '1', 'per_page': '1'},
+    );
+    var req = http.Request('get', url);
+    req.headers.addAll(
+      {
+        'Authorization': 'chave',
+      },
+    );
+    final result = await req.send();
+    if (result.statusCode == 200) {
+      final response = await http.Response.fromStream(result);
+      var decodedJSON = json.decode(response.body);
+
+      var imagem = ImageModel.fromJSON(decodedJSON);
+      print(imagem);
+    } else {
+      print('Falhou!');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,7 +47,7 @@ class AppState extends State <App> {
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             // numeroImagens++;
-            setState((){
+            setState(() {
               numeroImagens++;
             });
           },
