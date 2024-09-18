7fd78ee8902b29fe6ceb6cce2558b40b2851157e
Rodrigo Bossini <professorbossini@gmail.com>
2024-09-18 12:44:50 -0300

    g2-t3(flutter): pg 67
    - ja fizemos async/await
    - retomar criando uma lista de imageModel como variavel de estado

--- flutter/exibe_imagens/lib/src/app.dart
+++ flutter/exibe_imagens/lib/src/app.dart
@@ -1,30 +1,56 @@
+import 'package:exibe_imagens/src/models/image_model.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class App extends StatefulWidget{
-  State<App> createState(){
+class App extends StatefulWidget {
+  State<App> createState() {
     return AppState();
   }
 }
 
-class AppState extends State <App>{
+class AppState extends State<App> {
   int numeroImagens = 0;
-  @override
 
+  void obterImagem() async {
+    //https://api.pexels.com/v1/search?query=people&page=1&per_page=1
+    final url = Uri.https('api.pexels.com', '/v1/search',
+        {'query': 'people', 'page': '1', 'per_page': '1'});
+
+    final req = http.Request('get', url);
+
+    req.headers.addAll({
+      'Authorization':
+          'a91Qyfh2Ud1rdeOGKV8aTR5Aj9UmRvdma6EdyhC9EfKStoAyt7rmDuhV'
+    });
+
+    final result = await req.send();
+    final response = await http.Response.fromStream(result);
+    final mapa = json.decode(response.body);
+    final imagem = ImageModel.fromJSON(mapa);
+    // req.send().then((result) {
+    //   http.Response.fromStream(result).then((response) {
+    //     final mapa = json.decode(response.body);
+    //     final imagem = ImageModel.fromJSON(mapa);
+    //     print(imagem);
+    //   });
+    // });
+
+    //http.get(Uri.parse('url da pexels'));
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Minhas imagens"),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: (){
-            // numeroImagens++;
-            setState((){numeroImagens++;});
-          },
-          child: const Icon(Icons.add),
-        ),
-        body: Text('$numeroImagens'),
-      )
-    );
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Minhas imagens"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: obterImagem,
+        child: const Icon(Icons.add),
+      ),
+      body: Text('$numeroImagens'),
+    ));
   }
 }
--- flutter/exibe_imagens/lib/src/models/image_model.dart
+++ flutter/exibe_imagens/lib/src/models/image_model.dart
@@ -1,12 +1,16 @@
-class ImageModel{
+class ImageModel {
   late String url;
   late String alt;
 
   ImageModel(this.url, this.alt);
 
-  ImageModel.fromJSON(Map <String, dynamic> mapa){
+  ImageModel.fromJSON(Map<String, dynamic> mapa) {
     url = mapa['photos'][0]['src']['small'];
     alt = mapa['photos'][0]['alt'];
   }
-}
 
+  @override
+  String toString() {
+    return 'url: $url, alt: $alt';
+  }
+}
